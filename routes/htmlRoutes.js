d866c4c2643d8b6425f7632fa1e3ae98b2d34756
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-08 22:44:49 -0700

    set up the server

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path")
+
+module.exports = function(app) {
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/exercise.html'))
+});
+
+// GET Route for feedback page
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/stats.html'))
+});
+
+};
+
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const logger = require("morgan")
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", { useNewUrlParser: true });
+
+//const db = require("./models" 
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
